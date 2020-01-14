954c6483fe46b1f57516910360b2460b65db63ed
joboy19 <joeboyd9111@gmail.com>
2020-01-14 18:11:29 +0000

    tiles/items are lists of custom classes, added first item

--- game.py
+++ game.py
@@ -1,19 +1,79 @@
 import pygame, sys
 from pygame import *
 
-def read_level(filen):
+level_file_names = ["levels//1.txt"]
+
+class tile():
+    def __init__(self, img_t, pos):
+        self.img_t = img_t
+        self.pos = pos
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+
+    def render(self, scroll):
+        if self.img_t == "1":
+            screen.blit(grass, (self.pos[0]-scroll[0], self.pos[1]-scroll[1]))
+        if self.img_t == "2":
+            screen.blit(dirt, (self.pos[0]-scroll[0], self.pos[1]-scroll[1]))
+
+class item():
+    def __init__(self, t, pos):
+        self.t = t
+        self.pos = pos
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+        self.visable = True
+
+    def render(self, scroll):
+        if (self.visable == False):
+            return
+        if self.t == "B":
+            screen.blit(book1, (self.pos[0]-scroll[0], self.pos[1]-scroll[1]))
+        
+    def oncollide(self, player_rect):
+        self.visable = False
+        if self.t == "B":
+            player_items[0] = True
+        return
+
+
+
+def make_level_data(levelnum):
+    level_data = read_level(levelnum)
+    level_tiles = []
+    level_items = []
+    for x in enumerate(level_data):
+        for y in enumerate(x[1]):
+            #nothing
+            if y[1] == "0":
+                continue
+            #safe tiles
+            if y[1] == "1":
+                level_tiles.append(tile(y[1], (y[0]*16, x[0]*16)))
+            if y[1] == "2":
+                level_tiles.append(tile(y[1], (y[0]*16, x[0]*16)))
+            #items
+            if y[1] == "B":
+                level_items.append(item(y[1], (y[0]*16, x[0]*16)))
+    
+    return level_tiles, level_items
+
+
+def read_level(levelnum):
+    filen = level_file_names[levelnum]
     grid = []
     with open(filen, "r") as filee:
         for x in enumerate(filee):
             grid.append([]) 
-            row = x[1].strip().split(",")
+            row = x[1]
             for y in row:
                 grid[x[0]].append(y)
     return grid
 
+
 win_height = 400
 win_width = 608
 display = (win_width, win_height)
+
+scroll = [0, 0]
 
 pygame.init()
 pygame.display.set_caption("Treasure Hunter - Durham")
@@ -21,63 +81,79 @@ pygame.display.set_caption("Treasure Hunter - Durham")
 screen = pygame.display.set_mode(display, 0, 32)
 clock = pygame.time.Clock()
 
-background = pygame.image.load("img//background.png")
-player = pygame.image.load("img//player_tri.png")
-player.set_colorkey((255,255,255))
+background_img = pygame.image.load("img//background.png")
+player_img = pygame.image.load("img//player.png")
+player_fall_img = pygame.image.load("img//player_fall.png")
 grass = pygame.image.load("img//grass.png")
 dirt = pygame.image.load("img//dirt.png")
+book1 = pygame.image.load("img//book1.png")
 
 player_vert_mom = 0
-player_rect = pygame.Rect(50, 50, player.get_width(), player.get_height())
+player_size = [16, 32]
+player_rect = pygame.Rect(300, 200, player_size[0], player_size[1])
+player_items = [False]
 
-moving_right, moving_left = False, False
+moving_right, moving_left, facing_right = False, False, False
+in_air = True
+used_jump, used_jump2 = False, False
 
 #check if colliding with list of tiles
 def colliding_tiles(rectin, tiles):
     colliding_tiles = []
-    for x in tiles:
-        if rectin.colliderect(x):
-            colliding_tiles.append(x)
-    return colliding_tiles
+    indexes = []
+    for x in enumerate(tiles):
+        if rectin.colliderect(x[1]):
+            colliding_tiles.append(x[1])
+            indexes.append(x[0])
+    return colliding_tiles, indexes
 
 #check if collision after moving (tiles)
 def check_collision_and_move(rectin, tiles, movement):
+    collisions = [False, False, False, False]
+    #top, bottom, left, right
     rectin.x += movement[0]
-    collding_with = colliding_tiles(rectin, tiles)
+    collding_with, _ = colliding_tiles(rectin, tiles)
     for x in collding_with:
         if movement[0] > 0:
-            rectin.right = x.left 
+            rectin.right = x.left
+            collisions[3] = True
         elif movement[0] < 0:
             rectin.left = x.right
+            collisions[2] = True
     
     rectin.y += movement[1]
-    collding_with = colliding_tiles(rectin, tiles)
+    collding_with, _ = colliding_tiles(rectin, tiles)
 
     for x in collding_with:
         if movement[1] > 0:
             rectin.bottom = x.top 
+            collisions[1] = True
         elif movement[1] < 0:
             rectin.top = x.bottom
+            collisions[0] = True
 
-    return rectin
+    return rectin, collisions
 
-current_level = read_level("levels//1.txt")
+def check_item_collisions(rectin, items):
+    _, indexes = colliding_tiles(rectin, [items.rect for items in items])
+    return indexes
 
+
+current_level = 0
+level_tiles, level_items = make_level_data(current_level)
+    
+count = True
 while True:
-    screen.blit(background, (0,0))
-    screen.blit(player, (player_rect.x, player_rect.y))
+    screen.blit(background_img, (0,0))
 
-    level_tiles = []
+    scroll[0] += (player_rect.x - scroll[0] - 150)
+    scroll[1] += (player_rect.y - scroll[1] - 200)
 
-    for x in enumerate(current_level):
-        for y in enumerate(x[1]):
-            if y[1] == "0":
-                continue
-            if y[1] == "1":
-                screen.blit(grass, (y[0]*16, x[0]*16))
-            if y[1] == "2":
-                screen.blit(dirt, (y[0]*16, x[0]*16))
-            level_tiles.append(pygame.Rect(y[0]*16, x[0]*16, 16, 16))
+    for x in level_tiles:
+        x.render(scroll)
+    
+    for x in level_items:
+        x.render(scroll)
 
     player_move_tick = [0, 0]
 
@@ -92,28 +168,77 @@ while True:
 
     player_move_tick[1] += player_vert_mom
 
-    for event in pygame.event.get():
+    rects = [x.rect for x in level_tiles]
+    
 
+    player_rect, collisions = check_collision_and_move(player_rect, [x.rect for x in level_tiles], player_move_tick)
+
+    if collisions[1]:
+        used_jump = False
+        used_jump2 = False
+        in_air = False
+    
+    colliding_items = check_item_collisions(player_rect, level_items)
+    for x in colliding_items:
+        level_items[x].oncollide(player_rect)
+
+    for event in pygame.event.get():
         if event.type == QUIT:
             print("Game quit.")
             pygame.quit()
             sys.exit()
 
         if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
+            if event.key == K_d:
                 moving_right = True
-            if event.key == K_LEFT:
+                facing_right = True
+            if event.key == K_a:
                 moving_left = True
+                facing_right = False
             if event.key == K_SPACE:
-                player_vert_mom += -5
+                if used_jump and (not used_jump2):
+                    in_air = True
+                    player_vert_mom += -6.5
+                    used_jump2 = True
+                elif (not used_jump) and (not used_jump2):
+                    player_vert_mom += -5.5
+                    in_air = True
+                player_vert_mom = max(player_vert_mom, -3.5)
+                used_jump = True
+            if event.key == MOUSEBUTTONDOWN:
+                pos = pygame.mouse.getpos()
+                
+                
 
         if event.type == KEYUP:
-            if event.key == K_RIGHT:
+            if event.key == K_d:
                 moving_right = False
-            if event.key == K_LEFT:
+            if event.key == K_a:
                 moving_left = False
+    
+    if in_air:
+        if facing_right:
+            screen.blit(player_fall_img, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+        else:
+            screen.blit(pygame.transform.flip(player_fall_img, True, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+    else:
+        if facing_right:
+            screen.blit(player_img, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+        else:
+            screen.blit(pygame.transform.flip(player_img, True, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
-    player_rect = check_collision_and_move(player_rect, level_tiles, player_move_tick)
+        
+    
+    
+    if count % 100 == 0:
+        print("tick", count)
+    count+=1
+
+    myfont = pygame.font.SysFont("Arial", 10)
+    letter = myfont.render("fps: " + str(round(clock.get_fps(),2)),0,(0,0,0))
+    screen.blit(letter,(0,0))
+    letter = myfont.render("v_m: " + str(round(player_vert_mom,2)),0,(0,0,0))
+    screen.blit(letter,(0,10))
 
     
     pygame.display.update()
---
+++ test.py
@@ -0,0 +1,9 @@
+class test():
+    def __init__(self, meme):
+        self.meme = meme
+
+a = [1,2]
+y = test(a)
+print(y.meme)
+a = [1,2,3]
+print(y.meme)
